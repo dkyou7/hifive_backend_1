0cc0e32a9a19bdeae5cc1b17ae469adbfcfc907d
You Dong Kwan <dkyou7@naver.com>
2024-10-23 14:18:10 +0900

    first commit

---
+++ src/main/java/com/example/hifive_be_1/HifiveBe1Application.java
@@ -0,0 +1,13 @@
+package com.example.hifive_be_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifiveBe1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HifiveBe1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hifive_be_1/HifiveBe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hifive_be_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HifiveBe1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
